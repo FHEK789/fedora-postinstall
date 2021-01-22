552fcd077a97089a6d961d3d672cee6c953960ac
Felipe Kinoshita <kinofhek@gmail.com>
2021-01-21 23:49:32 -0300

    feat: change repo from packman to packman-essentials

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # add packman repositories
-sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials packman-essentials
 
-sudo zypper dup --from packman --allow-vendor-change
+sudo zypper dup --from packman-essentials --allow-vendor-change
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -13,7 +13,7 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 chsh -s /usr/bin/zsh
 
-sudo zypper up
+sudo zypper dup
 
 mkdir $HOME/www
 
