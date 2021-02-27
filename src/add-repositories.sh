4aa87ab3cde30cbcd926912f5043a2ff27b06ab8
Felipe Kinoshita <kinofhek@gmail.com>
2021-02-27 15:25:20 -0300

    feat: remove vscode repositorie

--- src/add-repositories.sh
+++ src/add-repositories.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# add vscode repository
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc --quiet
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
-
 sudo zypper refresh
