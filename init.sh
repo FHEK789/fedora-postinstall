9ff7a0a07cab2df6525b823a9d84d927f682595d
Felipe Kinoshita <kinofhek@gmail.com>
2021-03-02 01:17:33 -0300

    feat: move repositories to add-repositories.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# add packman repositories
-zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
-
-sudo zypper dup --from packman --allow-vendor-change
-
-sudo zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
-
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 ./src/add-repositories.sh
 
 ./src/software.sh
--- src/add-repositories.sh
+++ src/add-repositories.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-sudo zypper refresh
+# add packman repositories
+zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+
+# switch system packages to packman
+sudo zypper dup --from packman --allow-vendor-change
+
+# add NVIDIA repo
+sudo zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
+
+# add flathub repo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
